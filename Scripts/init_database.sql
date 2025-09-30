447f1dbfc353555109e2dd1acfc0a37e726daedf
Jihene Samet <111638092+JiheneSamet98@users.noreply.github.com>
2025-09-30 18:35:04 +0100

    Create init_database.sql

---
+++ Scripts/init_database.sql
@@ -0,0 +1,13 @@
+-------- Create data warehouse database and shema
+create database DateWarehouse;
+GO
+
+USE DataWarehouse
+GO
+
+----create schema
+create schema bronze
+GO
+create schema silver
+GO
+create schema Gold
